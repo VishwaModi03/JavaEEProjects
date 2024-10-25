11690d77d2c25b4c01552209180a33fefeb289ec
vishwa <vishwa@vishwa-VivoBook-ASUSLaptop-X415EA-X415EA>
2024-10-25 22:42:32 +0530

    First Commit!!

---
+++ src/main/java/ejb/statelessLogic.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/StatelessEjbClass.java to edit this template
+ */
+package ejb;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author vishwa
+ */
+@Stateless
+public class statelessLogic implements statelessLogicLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    int i;
+    
+    @PostConstruct
+    public void initialize()
+    {
+        i=0;
+    }
+
+    @Override
+    public int increment() {
+        //throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return i++;
+    }
+    
+}
